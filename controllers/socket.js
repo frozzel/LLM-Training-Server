db19b75339247f03d7a12d3b0d1310d237b2806c
Dennis Hickox <frozzel@me.com>
2025-01-09 16:41:25 -0500

    testing websocket server

--- controllers/socket.js
+++ controllers/socket.js
@@ -1,3 +1,4 @@
+const WebSocket = require("ws");
 
 
 ////////////// Test Socket Routes ///////////// 
@@ -10,3 +11,44 @@ exports.testApi = (req, res) => {
         </div><div style="width: 30%; height: auto"></div>
         </body>`);
 };
+
+////////////// Socket Server /////////////
+const url = "wss://api.openai.com/v1/realtime?model=gpt-4o-realtime-preview-2024-12-17";
+const ws = new WebSocket(url, {
+  headers: {
+    "Authorization": "Bearer " + process.env.OPENAI_API_KEY,
+    "OpenAI-Beta": "realtime=v1",
+  },
+});
+
+ws.on("open", function open() {
+  console.log("Connected to server.");
+});
+
+const message = {
+    type: "message",
+    content: "Hello, world!",
+  };
+  ws.on("open", function open() {
+    ws.send(JSON.stringify(message));
+    console.log("Sent message to server.");
+    console.log(message);
+  });
+  
+// // Server-sent events will come in as messages...
+// ws.on("message", function incoming(message) {
+//     // Message data payloads will need to be parsed from JSON:
+//     const serverEvent = JSON.parse(message.data)
+//     console.log(serverEvent);
+//   });
+  
+//   // To send events, create a JSON-serializeable data structure that
+//   // matches a client-side event (see API reference)
+//   const event = {
+//     type: "response.create",
+//     response: {
+//       modalities: ["audio", "text"],
+//       instructions: "Give me a haiku about code.",
+//     }
+//   };
+//   ws.send(JSON.stringify(event));
